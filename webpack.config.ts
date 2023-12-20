59a08d443080d24418cedcb7cf4220d3c2225b08
zaymovskey <zaymovskey@mail.com>
2023-12-20 15:50:10 +0300

    init + webpack + webpackdevserver

---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import type webpack from 'webpack';
+import webpackDevServer from 'webpack-dev-server';
+
+const config: webpack.Configuration = {
+  entry: './src/index.ts',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development'
+};
+
+export default config;

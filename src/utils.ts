b807f5ca83dff4d0dbf7765ded749ea9efcdfad2
zaymovskey <zaymovskey@mail.com>
2023-12-21 13:20:36 +0300

    get

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,14 @@
-export function randomIntFromRange(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+export function getRandomIntFromRange(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-export function randomColor(colors: string[]) {
-  return colors[Math.floor(Math.random() * colors.length)]
+export function getRandomColor(colors: string[]) {
+  return colors[Math.floor(Math.random() * colors.length)];
 }
 
-export function distance(x1: number, y1: number, x2: number, y2: number) {
-  const xDist = x2 - x1
-  const yDist = y2 - y1
+export function getDistance(x1: number, y1: number, x2: number, y2: number) {
+  const xDist = x2 - x1;
+  const yDist = y2 - y1;
 
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
